f18f50a251e293b6fd30e13704844ac90d556bed
sneha931 <cs22157@velammalitech.edu.in>
2025-12-18 18:21:25 +0530

    deployment

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,4 @@
 import serverless from "serverless-http";
-// Import from source - Vercel will compile TypeScript automatically
 import app from "../src/app";
 
-// Export handler for Vercel serverless functions
-export default serverless(app);
+export const handler = serverless(app);
